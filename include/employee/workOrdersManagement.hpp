996511d7509eaeb07a26f6a7bc2b4127c36e118b
aleksandarstanisic-source <aleksandar.stanisic@student.etf.unibl.org>
2026-01-14 12:27:15 +0100

    no message

--- include/employee/workOrdersManagement.hpp
+++ include/employee/workOrdersManagement.hpp
@@ -20,6 +20,7 @@ class WorkOrderManager {
         bool searchForWorkOrder (int workOrderId);
         void listCompletedWorkOrders ();
         bool isWorkOrderCompleted (int workOrderId);
+        void generateWorkOrderTXTFile (int workOrderId);
 
     private:
         std::string statusToString (WorkOrderStatus status);
